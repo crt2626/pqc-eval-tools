20054036c9a51fdc7e667ec51c92e92f729cdce0
40428470 <40428470@live.napier.ac.uk>
2023-02-12 21:32:22 +0000

    modifed build script to check for and install depe

--- Scripts/x86-linux-build.sh
+++ Scripts/x86-linux-build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-#Setting up directory and building liboqs
+
+# Check for and install required packages
+packages=(astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind)
+not_installed=()
+for package in "${packages[@]}"; do
+    if ! dpkg -s "$package" >/dev/null 2>&1; then
+        not_installed+=("$package")
+    fi
+done
+if [[ ${#not_installed[@]} -ne 0 ]]; then
+    sudo apt-get update
+    sudo apt-get install "${not_installed[@]}"
+fi
+
+# Setting up directory and building liboqs
 cd ../liboqs
 mkdir x86-liboqs-linux
 cd x86-liboqs-linux
@@ -7,6 +21,6 @@ cmake -GNinja .. -DCMAKE_INSTALL_PREFIX=./
 ninja -j 4
 ninja install
 
-#Moving build direcotry 
+# Moving build directory
 cd ../
 mv x86-liboqs-linux ../Builds
