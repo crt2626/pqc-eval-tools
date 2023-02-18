64eecb79996bc5dad849490477a01fbba84482c2
40428470 <40428470@live.napier.ac.uk>
2023-02-18 17:30:02 +0000

    d

--- download-script-linux.sh
+++ download-script-linux.sh
@@ -5,7 +5,7 @@ Downloading and confiuring repo
 function download() {
 
     # Function for setting up the repo
-    mkdir /pqc && cd /pqc && git clone https://github.com/crt2626/pqc-eval-tools.git
+    sudo mkdir /pqc && cd /pqc && git clone https://github.com/crt2626/pqc-eval-tools.git
     cd /pqc/pqc-eval-tools/scripts
     chmod +x *.sh
 
