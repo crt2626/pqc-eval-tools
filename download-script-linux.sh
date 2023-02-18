80069593916f1bb758f6b029019567b2de574f27
40428470 <40428470@live.napier.ac.uk>
2023-02-18 21:02:56 +0000

    tets

--- download-script-linux.sh
+++ download-script-linux.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+: '
+Downloading and confiuring repo
+'
 
-# Downloading and configuring repo
 function download() {
+
     # Function for setting up the repo
-    sudo mkdir /pqc && cd /pqc && git clone https://github.com/crt2626/pqc-eval-tools.git
-    sudo chown $USER:$USER /pqc
+    sudo mkdir /pqc && cd /pqc && sudo git clone https://github.com/crt2626/pqc-eval-tools.git
+    sudo chown -R $USER:$USER /pqc
+    sudo chmod -R 755 /pqc
+
     cd /pqc/pqc-eval-tools/scripts
-    sudo chmod +x /pqc/pqc-eval-tools/*.sh
+    chmod +x /pqc/pqc-eval-tools/*.sh
+
 }
 
 # Checking if the directory is already there
