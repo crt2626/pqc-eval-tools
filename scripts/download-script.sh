c66849ea26dfe87a7ab647094d1690a3ddb8d7f6
40428470 <40428470@live.napier.ac.uk>
2023-02-18 15:30:39 +0000

    sds

--- scripts/download-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-: '
-Downloading and confiuring repo
-'
-# Working 
-mkdir /pqc && cd /pqc && git clone https://github.com/crt2626/pqc-eval-tools.git
-cd /pqc/pqc-eval-tools/scripts
-chmod +x *.sh
-
