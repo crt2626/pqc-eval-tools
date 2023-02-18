6606efc6571a2872f5ddb58cbec0d97337f04a3e
40428470 <40428470@live.napier.ac.uk>
2023-02-18 00:05:17 +0000

    test

--- scripts/run-all-tests.sh
+++ scripts/run-all-tests.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-
 : '
 Performing setup of test suite
 '
+# configuring scripts
+chmod +x *.sh
+
+#Checking system type and building
+if [ "$(uname -m)" = "x86_64" ] && [ "$(uname -s)" = "Linux" ]; 
+then
+    # x86 Linux
+    ./x86-linux-build.sh
+
+elif [ "$(uname -m)" = "armv7l" ]; 
+then
+  # ARM
+  ./arm-linux-build.sh
+  
+else
+    #Unsuppoutred system
+    echo "Unsupported architecture or operating system for this script"
+    exit 1
+fi
+
+
 # Creating unparsed results directory and clearing old results if present
 if [ -d "../up-results" ];
 then 
