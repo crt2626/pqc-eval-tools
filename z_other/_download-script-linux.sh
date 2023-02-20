3b79276c5c3990b0eeea444e990a134bad8db2c1
40428470 <40428470@live.napier.ac.uk>
2023-02-20 21:16:07 +0000

    dsf

--- z_other/_download-script-linux.sh
+++ z_other/_download-script-linux.sh
@@ -15,13 +15,32 @@ function download() {
 
 }
 
+function dependency_install() {
+
+    # Check for and install required packages
+    packages=(astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind)
+    not_installed=()
+    for package in "${packages[@]}"; do
+        if ! dpkg -s "$package" >/dev/null 2>&1; then
+            not_installed+=("$package")
+        fi
+    done
+    if [[ ${#not_installed[@]} -ne 0 ]]; then
+        sudo apt-get update
+        sudo apt-get install "${not_installed[@]}"
+    fi
+
+}
+
 # Checking if the directory is already there
 if [ -d "/pqc" ];
 then 
     sudo rm -r /pqc
     download
+    dependency_install
 else
     download
+    dependency_install
 fi
 
 echo -e "\nRepo has been setup in the /pqc directory\n"
