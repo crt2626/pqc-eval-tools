a4555115224842b8b5facc26a23c3521e74a0365
40428470 <40428470@live.napier.ac.uk>
2023-02-20 15:38:50 +0000

    ffhg

--- download-script-linux.sh
+++ download-script-linux.sh
@@ -5,16 +5,6 @@ Downloading and confiuring repo
 
 function download() {
 
-    # Checking if git is installed
-    if command -v git >/dev/null 2>&1 ; 
-    then
-        echo -e  "Git is installed - skiping install\n"
-        sudo apt install git -y
-    else
-        echo "Git is not installed - installing git\n"
-        sudo apt install git -y
-    fi
-
     # Function for setting up the repo
     sudo mkdir /pqc && cd /pqc && sudo git clone https://github.com/crt2626/pqc-eval-tools.git
     sudo chown -R $USER:$USER /pqc
