794ed37796e544e25bb099cb0744f91db836abda
40428470 <40428470@live.napier.ac.uk>
2023-02-23 18:43:34 +0000

    fgfg

--- result-processing/parsing-scripts/avg_gen.py
+++ result-processing/parsing-scripts/avg_gen.py
@@ -46,39 +46,85 @@ def avg_mem(type_mem_dir):
     # Declaring dataframes and fieldnames
     mem_fieldnames = ["Algorithm", "Operation", "intits", "maxBytes", "maxHeap", "extHeap", "maxStack"]
     kem_mem_avg = pd.DataFrame(columns=mem_fieldnames)
+    sig_mem_avg = pd.DataFrame(columns=mem_fieldnames)
 
+    """Calculating KEM Averages"""
     # Looping through the kem algorithms
     for kem_alg in kem_algs:
 
-        temp_df1 = pd.DataFrame(columns=mem_fieldnames)
+        # Creating combined operations dataframe for current algorithm
+        combined_operations = pd.DataFrame(columns=mem_fieldnames)
 
         # Looping throuhg the run files
         for run_count in range(1,16,1):
 
             # Setting filename and reading csv into dataframe
             kem_mem_filename = kem_mem_file_prefix + str(run_count) + ".csv"
-            temp_df2 = pd.read_csv(kem_mem_filename)
+            temp_df = pd.read_csv(kem_mem_filename)
 
             #Getting the algorithm operations across all files into one
-            temp_df2 = temp_df2.loc[temp_df2["Algorithm"].str.contains(kem_alg)]
-            temp_df1 = pd.concat([temp_df2, temp_df1], ignore_index=True, sort=False)
+            temp_df = temp_df.loc[temp_df["Algorithm"].str.contains(kem_alg)]
+            combined_operations = pd.concat([temp_df, combined_operations], ignore_index=True, sort=False)
 
         #Getting the avereages for each operation
         for operation in kem_operations:
             
             # Getting a list of the operation metric averages 
-            temp_df3 = temp_df1.loc[temp_df1["Operation"].str.contains(operation)]
-            temp_df3 = temp_df3[mem_fieldnames[2:]]
-            temp_df3 = (temp_df3.mean(axis=0)).to_frame()
-            row = temp_df3.iloc[:, 0].to_list()
+            operation_average = combined_operations.loc[combined_operations["Operation"].str.contains(operation)]
+            operation_avergae = operation_avergae[mem_fieldnames[2:]]
 
-            #Creating new row and exporting to main memory average dataframe
+            # Calcualting Averages
+            operation_avergae = (operation_average.mean(axis=0)).to_frame()
+
+            #Creating new row and exporting to main kem memory average dataframe
+            row = operation_average.iloc[:, 0].to_list()
             row.insert(0, kem_alg)
-            row.insert(1,operation)
+            row.insert(1, operation)
             kem_mem_avg.loc[len(kem_mem_avg)] = row
 
-    csv_name = os.path.join(type_mem_dir,"kem-avg.csv")
-    kem_mem_avg.to_csv(csv_name, index=False)
+
+    """Calculating Digital Signature Averages"""
+    # Looping throuhg digital signature algorithms
+    for sig_alg in sig_algs:
+
+        # Creating combined operations dataframe for current algorithm
+        combined_operations = pd.DataFrame(columns=mem_fieldnames)
+
+        # Looping through run files
+        for run_count in range(1,16,1):
+
+            # Setting the filename and reading csv into dataframe
+            sig_mem_filename = sig_mem_file_prefix + str(run_count) + ".csv"
+            temp_df = pd.read_csv(sig_mem_filename)
+
+            # Geting the algorithm operations across all files into one
+            temp_df = temp_df.loc[temp_df["Algorithm"].str.contains(sig_alg)]
+            combined_operations = pd.concat([temp_df, combined_operations], ignore_index=True, sort=False)
+        
+        
+        # Getting the averages for each operation
+        for operation in sig_operations:
+
+            # Creating a list of the operation metric averages
+            operation_average = combined_operations.loc[combined_operations["Operation"].str.contains(operation)]
+            operation_average = operation_average[mem_fieldnames[2:]]
+
+            # Calculating averages
+            operation_average = (operation_average.mean(axis=0)).to_frame()
+
+            # Creating new row and exporting to main digital signature memory average dataframe
+            row = operation_average.iloc[:, 0].to_list()
+            row.insert(0, sig_alg)
+            row.insert(1, operation)
+            sig_mem_avg.loc[len(sig_mem_avg)] = row
+
+
+    # Exporting average csv files
+    kem_csv_name = os.path.join(type_mem_dir, "kem-mem-avg.csv")
+    kem_mem_avg.to_csv(kem_csv_name, index=False)
+    sig_csv_name = os.path.join(type_mem_dir, "sig-mem-avg.csv")
+    sig_mem_avg.to_csv(sig_csv_name, index=False)
+
 
 #***********************************************************************  
 def gen_averages(type_speed_dir, type_mem_dir, dir):
