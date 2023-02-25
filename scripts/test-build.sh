43d37114bc7c21b07cd0f7263ef5f1b8e3f86c38
40428470 <40428470@live.napier.ac.uk>
2023-02-25 16:44:57 +0000

    df

---
+++ scripts/test-build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Directory path variables
+root_dir="/pqc/pqc-eval-tools"
+build_dir="x86-liboqs-linux"
+
+# Check for and install required packages
+packages=(astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind python3-pip)
+not_installed=()
+for package in "${packages[@]}"; do
+    if ! dpkg -s "$package" >/dev/null 2>&1; then
+        not_installed+=("$package")
+    fi
+done
+if [[ ${#not_installed[@]} -ne 0 ]]; then
+    sudo apt-get update
+    sudo apt-get install -y "${not_installed[@]}" 
+fi
+
+# Setting up build directory and building liboqs
+cd "$root_dir"/liboqs && mkdir "$build_dir" && cd "$build_dir"
+#cmake -GNinja .. -DCMAKE_INSTALL_PREFIX=./
+cmake .. -DCMAKE_INSTALL_PREFIX=./ && make -j 4 && make install
+ninja -j 4
+ninja install
+
+# Making directory for this build and moving
+cd .. 
+mv "$root_dir"/liboqs/x86-liboqs-linux "$root_dir"/builds/.
+cd "$root_dir"/scripts
