d115b17572eb159732ed2e75074b48fa2b89e07b
40428470 <40428470@live.napier.ac.uk>
2023-02-25 18:24:13 +0000

    dsd

--- z_other/run_mem.py
+++ z_other/run_mem.py
@@ -1,118 +1,152 @@
+"""Imports"""
 import json
 import re
 import subprocess
 import sys
 import os
-from get_cpuinfo import getcpuinfo
-from get_cpuinfo import getestimatedcpufrequency
-data = {}
-# fetch both x86 and aarch64 CPU details:
-data["cpuinfo"]=getcpuinfo(["flags", "model name", "cpu MHz", "Features", "CPU implementer", "CPU variant", "CPU part", "BogoMIPS"])
-data["config"]={}
-estfrequency = getestimatedcpufrequency()
-if estfrequency != None:
-    data["cpuinfo"]["estfrequency"] = estfrequency
-else:
-    data["cpuinfo"]["estfreqency"] = "Unavailable"
+import shutil
 
+"""Global Variables"""
+data = {}
+fieldname=["insts", "maxBytes", "maxHeap", "extHeap", "maxStack"]
+root_dir = "/pqc/pqc-docker"
+output_dir = "/pqc/output"
+algs_dir = "/pqc/pqc-docker/algs/"
+kem_algs = []
+sig_algs = []
+
+#***********************************************************************
+def get_algs():
+    """Function for creating list of algorithms"""
+
+    # Setting alg text file directories
+    kem_algs_file = algs_dir + "kem-algs-list.txt"
+    sig_algs_file = algs_dir + "sig-algs-list.txt"
+
+    # # Getting the kem algs
+    with open(kem_algs_file, "r") as kem_file:
+
+        for line in kem_file:
+            kem_algs.append(line.strip())
+    
+    # Getting the digital siganture algorithms
+    with open(sig_algs_file, "r") as alg_file:
+
+        for line in alg_file:
+            sig_algs.append(line.strip())
+
+
+#*******************************************************************
 def get_peak(lines):
+    """Function for getting the peak memory metrics"""
+
+    # Setting the intinal peak value
     peak = -1
+
+    # Looping through output file lines looking for peak
     for line in lines: 
+        
+        # Getting snapshot number that has peak values
         if line.startswith(" Detailed snapshots: ["):
             match=re.search("\d+ \(peak\).*", line)
+
             if match:
                 peak = int(match.group(0).split()[0])
+
+         # Getting the line the file that has the peak metrics 
         if (peak > 0):
+            
             if line.startswith('{: >3d}'.format(peak)): # remove "," and print all numbers except first:
+                
                 nl = line.replace(",", "")
                 res = nl.split()
                 del res[0]
                 #print(" ".join(res))
                 return res
 
-def parse_config(output):
-   lines = output.splitlines()
-   loading = False
-   for line in lines:
-      if loading:
-         if line == "\n": # done
-            return
-         elif line.startswith("Started at"):
-                data["config"]["start"] = line[len("Started at "):]
-         elif ":" in line:
-                data["config"][line[:line.index(":")]]=line[line.index(":")+1:].lstrip()
-      elif line.startswith("====="):
-         loading=True
+#*******************************************************************
+def do_test(alg, meth, exepath):
+   """Performing the tests and outputing the results"""
 
-
-fieldname=["insts", "maxBytes", "maxHeap", "extHeap", "maxStack"]
-
-def do_test(alg, meth, methnames, exepath):
+   # Running the valgrind memory profiler and saving output
    process = subprocess.Popen(["valgrind", "--tool=massif", "--stacks=yes", "--massif-out-file=valgrind-out", exepath, alg, str(meth)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
    (outs, errs) = process.communicate()
+
+
+   # Copying the valgrin.out file
+   val_out_filename = output_dir + "/" + alg + "-" + "valgrind.out"
+   #shutil.copyfile("valgrind-out", val_out_filename)
+
+   # Valgrind exception handling
    if process.returncode != 0:
+
       print("Valgrind died with retcode %d and \n%s\n%s\nFatal error. Exiting." % (process.returncode, outs, errs))
       exit(1)
-   if len(data["config"]) == 0:
-      parse_config(outs)
+
+   # Parsing valgrind.out file through ms_print
    process = subprocess.Popen(["ms_print", "valgrind-out"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
    (outs, errs) = process.communicate()
+
+   # Copy ms_print output
+   ms_out_filename = output_dir + "/" + alg  + "-" + "ms-out.txt"
+   with open(ms_out_filename, "w") as file:
+       file.write(outs)
+
+   # Parsing memory metrics
    result = get_peak(outs.splitlines())
-   data[alg][methnames[meth]] = {}
+
+   # Printing out test results to terminal
    try: 
       print("Result for %s: %s" % (alg, " ".join(result)))
-      for i in range(5):
-         data[alg][methnames[meth]][fieldname[i]] = result[i]
+
    except TypeError:
       print("Result for %s: " % (alg))
       print(result)
       print(outs.splitlines())
 
-if len(sys.argv) != 2:
-   print("python3 %s <testprogram>" % (sys.argv[0]))
-   exit(1)
+#*******************************************************************
+def main ():
+   """Main function that sets up and execute tests"""
 
-exepath=sys.argv[1]
+   # Checking if enough arguments have been passed to the script
+   if len(sys.argv) != 2:
+      print("Not enough arguments")
+      exit(1)
 
-if exepath.find("kem")>0:
-   methnames=["keygen","encaps","decaps"]
-else:
-   methnames=["keygen","sign","verify"]
+   # Setting the input variables
+   exepath=sys.argv[1]
 
-try:
-   os.mkdir("build")
-   os.mkdir(os.path.join("build", "mem-benchmark"))
-except FileExistsError:
-   activealgs=[]
+   # Getings algs
+   get_algs()
 
-# first determine all enabled algorithms
-process = subprocess.Popen([exepath], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
-(outs, errs) = process.communicate()
-for line in outs.splitlines():
-   print(line)
-   if line.startswith("  algname: "):
-      algs = line[len("  algname: "):].split(", ")
+   # Setting the method names based on the test progamme being supplied
+   if exepath.find("kem") > 0:
 
-activealgs=[]
-# weed out algs not enabled
-for alg in algs:
-   process = subprocess.Popen([exepath, alg, "0"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
-   (outs, errs) = process.communicate()
-   enabled=True
-   for line in outs.splitlines():
-      if "not enabled" in line:
-         enabled=False
-   if enabled:
-         activealgs.append(alg)
+      #methnames=["keygen","encaps","decaps"]
+      algs = kem_algs
 
-for alg in activealgs:
-   data[alg]={}
-   # Activate this for a quick test:
-   #if alg=="DEFAULT":
-   for i in range(3):
-      do_test(alg, i, methnames, exepath)
+   else:
 
-# Dump data
-with open(exepath+".json", 'w') as outfile:
-    json.dump(data, outfile)
+      #methnames=["keygen","sign","verify"]
+      algs = sig_algs
 
+   # Looping throuhg algs and running the tests
+   for alg in algs:
+      # looping throuhg operations
+       for op in range(3):
+           do_test(alg, op, exepath)
+
+       temp_dir = "tmp"
+       contents = os.listdir(temp_dir)
+       for item in contents:
+           item_path = os.path.join(temp_dir, item)
+           if os.path.isfile(item_path):
+               os.remove(item_path)
+           else:
+               shutil.rmtree(item_path)
+
+#*******************************************************************
+
+"""Boiler Plate"""
+if __name__ == "__main__":
+    main()
