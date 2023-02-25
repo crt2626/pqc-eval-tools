4ada963a6745fc535d55a3b5e0a8e47d31483783
40428470 <40428470@live.napier.ac.uk>
2023-02-25 17:06:43 +0000

    sd

---
+++ test-setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function dependency_install() {
+
+    # Check for and install required packages
+    packages=(astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind python3-pip)
+    not_installed=()
+    for package in "${packages[@]}"; do
+        if ! dpkg -s "$package" >/dev/null 2>&1; then
+            not_installed+=("$package")
+        fi
+    done
+    if [[ ${#not_installed[@]} -ne 0 ]]; then
+        apt-get update
+        apt-get install -y "${not_installed[@]}"
+    fi
+
+}
+
+function download() {
+
+    # Function for setting up the repo
+    mkdir -p /pqc/output 
+    cd /pqc
+    git clone https://github.com/crt2626/pqc-docker
+    cd /pqc/pqc-docker
+    git clone https://github.com/open-quantum-safe/liboqs.git
+    chown -R $USER /pqc/
+    chmod -R 755 /pqc
+
+    cd /pqc/pqc-eval-tools/scripts
+    chmod +x /pqc/pqc-eval-tools/*.sh
+
+}
+
+function build() {
+
+    # Function for building and moving liboqs
+    cd /pqc/pqc-docker/liboqs
+    mkdir build && cd build && cmake .. -DCMAKE_INSTALL_PREFIX=./ && make -j 4 && make install
+    cd /pqc/pqc-docker/liboqs
+    mv build /pqc/pqc-docker
+
+    # Moving python test script executables folder
+    cp /pqc/pqc-docker/scripts/run_mem.py /pqc/pqc-docker/build
+    cd ~/
+}
+
+echo -e "\nSetting up testing enviroment..."
+dependency_install
+download
+build
